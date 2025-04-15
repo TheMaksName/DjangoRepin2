0e53e41b0f5fa439c86a334519edf2ce041febe0
TheMaksName <mlebedev740@gmail.com>
2025-04-15 23:38:46 +0400

    <main> Init commit

--- registration/registration/settings.py
+++ registration/registration/settings.py
@@ -135,7 +135,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/django-static/'
+STATIC_URL = '/static/'
+
+# STATIC_URL = '/django-static/'
 # STATIC_ROOT = '/app/registration/staticfiles'  # Папка, куда собирается статика
 # STATICFILES_DIRS = ['/app/registration/reg/static/',]
 
