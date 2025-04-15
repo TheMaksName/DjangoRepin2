2ee77c158deac665f947499fd93b6704aba077dc
TheMaksName <mlebedev740@gmail.com>
2025-04-15 23:45:43 +0400

    <main> Init commit

--- registration/registration/settings.py
+++ registration/registration/settings.py
@@ -135,11 +135,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
 
-# STATIC_URL = '/django-static/'
-# STATIC_ROOT = '/app/registration/staticfiles'  # Папка, куда собирается статика
-# STATICFILES_DIRS = ['/app/registration/reg/static/',]
+STATIC_URL = '/django-static/'
+STATIC_ROOT = '/app/registration/staticfiles'  # Папка, куда собирается статика
+STATICFILES_DIRS = ['/app/registration/reg/static/',]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
