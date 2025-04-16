f52c51e38b676a0578533e4a860d80ac239835f5
TheMaksName <149165905+TheMaksName@users.noreply.github.com>
2025-04-16 12:09:42 +0400

    Update settings.py

--- registration/registration/settings.py
+++ registration/registration/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&i1z@+ur)aj*hg+50_#w@=n7v)!iw@&*s2+c%)n^=4zv0a-e^9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['www.samlit.net', "samlit.net", "95.163.221.227", '0.0.0.0', "127.0.0.1"]
+ALLOWED_HOSTS = ['www.samlit.net', "samlit.net", "95.163.221.227", '0.0.0.0', "127.0.0.1", 'localhost']
 
 
 # Application definition
