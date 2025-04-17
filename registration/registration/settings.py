73d45d04e3ebc68aae388b3cca161d5bb98775b7
TheMaksName <149165905+TheMaksName@users.noreply.github.com>
2025-04-17 15:24:46 +0400

    Update settings.py

--- registration/registration/settings.py
+++ registration/registration/settings.py
@@ -149,7 +149,7 @@ USE_TZ = True
 
 
 USE_X_FORWARDED_HOST = True
-CSRF_TRUSTED_ORIGINS = ['www.samlit.net', 'samlit.net', 'http://95.163.221.227:8080']
+CSRF_TRUSTED_ORIGINS = ['http://www.samlit.net', 'http://samlit.net', 'http://95.163.221.227:8080']
 
 STATIC_URL = '/django_static/'
 STATIC_ROOT = '/app/staticfiles/'  # Папка, куда собирается статика
